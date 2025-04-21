af3df4bf1c869d47dbc5e00cf9bbde23359db943
Hojjat Kaveh <hojjatks@gmail.com>
2025-04-21 16:56:03 -0700

    Reducing Nt so that now the g1 is behaving better

--- Codes/earthquake/GenData.py
+++ Codes/earthquake/GenData.py
@@ -12,14 +12,16 @@ index = int(sys.argv[1])
 np.random.seed(index)  # seed is set using the index
 import time
 start_time = time.time()
+from scipy.interpolate import interp1d
+
 #%%
 # load the long simulation.
 drs=0.012
 T_final_load=10500
-ApplyPOD=False # 1 if you want to apply POD, 0 if you want to load the data
+ApplyPOD=False             # True if you want to apply POD, 0 if you want to load the data
 Nxout=cte_eq.Nxout
 Ntout=cte_eq.Ntout
-T_filter=500 
+T_filter=7500 
 
 #%%
 if ApplyPOD:
@@ -29,7 +31,9 @@ if ApplyPOD:
     theta=data['theta']
     dc=data['dc']
     t=data['t']
-    Nx=dc.shape[0]
+    Nx=int(dc.shape[0]//4)
+    print('Nx=',Nx)
+
     U,S,VT,q_bar=ApplyPODStateSpace2D(v,theta,t,T_filter,Nx)
     # saving U,S,VT,q_bar to later use in if ApplyPOD==0
     np.savez('/central/groups/astuart/hkaveh/Data/LearnROM/transfer/ApplyPOD_V_theta_together'+str(T_final_load)+"Nx="+str(Nxout)+"Nt="+str(Ntout)+'drs'+str(drs),U=U,S=S,VT=VT,q_bar=q_bar)
@@ -62,7 +66,32 @@ while v0max>2e1: # making sure the initial condition is somewhere that has less 
     v0max=np.max(v0)
 
 #%%
-p=forwardmodel(T_final_run,cte_eq.Ntout,cte_eq.Nxout,Specifyinit,u_init,drs)
+v_init_lowres=u_init[:Nx]
+theta_init_lowres=u_init[Nx:]
+x_lowres=np.linspace(-cte_eq.L,cte_eq.L,Nx)
+x_highres=np.linspace(-cte_eq.L,cte_eq.L,Nx*cte_eq.Nxout)
+
+
+#%%
+v_interp = interp1d(x_lowres, v_init_lowres.flatten(), kind='linear', fill_value="extrapolate")
+theta_interp = interp1d(x_lowres, theta_init_lowres.flatten(), kind='linear', fill_value="extrapolate")
+
+# Interpolated high-res fields
+v_init_highres = v_interp(x_highres).reshape(-1, 1)
+theta_init_highres = theta_interp(x_highres).reshape(-1, 1)
+# Combine into new u_init
+u_init_highres = np.vstack([v_init_highres, theta_init_highres])
+
+
+#%%
+
+
+
+
+
+
+#%%
+p=forwardmodel(T_final_run,cte_eq.Ntout,cte_eq.Nxout,Specifyinit,u_init_highres,drs)
 #%%
 direct='/central/groups/astuart/hkaveh/Data/LearnROM/transfer/SampleSimulation_Tf_2D'+str(T_final_run)+"Nt="+str(cte_eq.Ntout)+"N_m"+str(N_m)+"coeff"+str(coeff)+"number"+str(index)
 # You only need time, V, theta:
--- Codes/earthquake/cte_eq.py
+++ Codes/earthquake/cte_eq.py
@@ -1,4 +1,4 @@
-Ntout=300
-Nxout=1
+Ntout=50
+Nxout=4
 L=240e3
 t_yr = 3600 * 24 * 365.25   # seconds per 
